630f39694377f3473d58ad1243c72a806fc3d293
umark <leenkhan@outlook.com>
2018-06-03 14:12:18 +0800

    新增排行

--- chapter2/recommend_test.py
+++ chapter2/recommend_test.py
@@ -1,15 +1,21 @@
 import recommendations as recommend
 
-print recommend.critics['Toby']
+#print recommend.critics['Toby']
 
-print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Gene Seymour')
+# print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Gene Seymour')
+#
+# print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Michael Phillips')
+#
+# print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Claudia Puig')
+#
+# print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Mick LaSalle')
+#
+# print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Jack Matthews')
+#
+# print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Toby')
 
-print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Michael Phillips')
+#print recommend.sim_pearson(recommend.critics, 'Lisa Rose' ,'Gene Seymour')
 
-print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Claudia Puig')
+print recommend.topMatches(recommend.critics, 'Toby', n=3)
 
-print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Mick LaSalle')
-
-print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Jack Matthews')
-
-print recommend.sim_distance(recommend.critics, 'Lisa Rose' ,'Toby')
+print recommend.topMatches(recommend.critics, 'Toby', n=3, similarity=recommend.sim_distance)
--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -6,7 +6,7 @@ critics ={'Lisa Rose':{'Lady in the Water':2.5, 'Snakes on a Plane':3.5,'Just My
                        'You, Me and Dupress':2.5,'The night Listener':3.0},
           'Gene Seymour':{'Lady in the Water':3.0, 'Snakes on a Plane':3.5,'Just My Luck':1.5,'Superman  Returns':5.0,
                        'You, Me and Dupress':3.5,'The night Listener':3.0},
-          'Michael Phillips':{'Lady in the Water':2.5, 'Snakes on a Plane':4.0,'Just My Luck':1.5,'Superman  Returns':3.5,
+          'Michael Phillips':{'Lady in the Water':2.5, 'Snakes on a Plane':3.0,'Superman  Returns':3.5,
                        'The night Listener':4.0},
           'Claudia Puig':{ 'Snakes on a Plane':3.5,'Just My Luck':3.0,'Superman  Returns':4.0,
                        'The night Listener':4.5,'You, Me and Dupress':2.5},
@@ -30,3 +30,39 @@ def sim_distance(prefs, person1, person2):
     sum_of_squares=sum(pow(prefs[person1][item] - prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2])
 
     return 1/(1+sqrt(sum_of_squares))
+
+
+
+
+#返回p1和p2的皮尔逊相关系数
+def sim_pearson(prefs, p1, p2):
+    si={}
+    for item in prefs[p1]:
+        if item in prefs[p2]: si[item]=1
+
+    n=len(si)
+
+    if n==0: return 1
+
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    sum1sq=sum([pow(prefs[p1][it],2) for it in si])
+    sum2sq=sum([pow(prefs[p2][it],2) for it in si])
+
+    pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+    num=pSum - (sum1*sum2/n)
+    den=sqrt((sum1sq-pow(sum1, 2)/n)*(sum2sq-pow(sum2, 2)/n))
+    if den==0: return 0
+
+    r=num/den
+
+    return r
+
+def topMatches(perfs, person, n=5, similarity=sim_pearson):
+    scores=[(similarity(perfs, person, other),other) for other in perfs if other!=person]
+
+    scores.sort()
+    scores.reverse()
+    return  scores[0:n]
